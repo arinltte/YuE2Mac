8d09b692de7af6d70be6bd317ed7fff8bf4f9ee6
arinltte <166289099+arinltte@users.noreply.github.com>
2026-09-14 18:51:18 +0800

    Initial Commit

---
+++ YuE2Mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YuE2Mac
+//
+//  Created by Chen Jin Shen on 14/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YuE2Mac/YuE2MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuE2MacApp.swift
+//  YuE2Mac
+//
+//  Created by Chen Jin Shen on 14/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct YuE2MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
